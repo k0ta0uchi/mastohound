7f5322c3a50275589141c3b82a93e33e33df764d
k0ta0uchi <k0ta0uchi@gmail.com>
2017-05-28 19:29:18 +0900

    check followees to avoid double follow.

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const fs = require('fs');
 const Masto = require('mastodon');
 
+// get prefs from pref.json. (use pref.template.json as a template)
+// first, use auth.js to get instances' access_tokens.
 const pref = JSON.parse(fs.readFileSync('./pref.json', 'utf8'));
-const interval_min = 3;
-const status_limit = 5;
 
+const INTERVAL_MIN = 1;         // interval for following.
+const STATUS_LIMIT = 5;         // number of statuses to process at once.
+const FOLLOWINGS_LIMIT = 80;    // number of following limit when retrieving accounts.
+
+// target instance to follow
 let targetM = new Masto({
     access_token: pref.target.access_token,
     timeout_ms: 60 * 1000,
     api_url: 'https://' + pref.target.domain + '/api/v1/',
 });
 
+// following account
 let myM;
+// if single mode, use target account as following account
 if( !pref.singlemode )
 {
     myM = new Masto({
@@ -24,15 +31,97 @@ if( !pref.singlemode )
     myM = targetM;
 }
 
-setInterval(() => {
-    targetM.get('timelines/public', {local:true,limit:status_limit})
+// get account's id to follow.
+myM.get('accounts/verify_credentials',{})
+.then(res => {
+    getFollowings(res.data.id, []);
+});
+
+/**
+ * get all following accounts and put into an array.
+ * @param {number} id      // account's id to follow.
+ * @param {array} accounts // following accounts.
+ * @param {number} max_id  // next page's max_id. (option)
+ */
+var getFollowings = (id, accounts, max_id) => {
+    console.log('max_id: '+ max_id);
+
+    if(max_id == undefined)
+    {
+        max_id = '';
+    }
+    // get following accounts by page
+    myM.get('accounts/' + id + '/following',{
+        max_id: max_id,
+        limit: FOLLOWINGS_LIMIT
+    })
+    .then(res => {
+        accounts = accounts.concat(res.data);
+
+        // scrape next page's max_id.
+        /.*?max_id=(\d*)?>/.exec(res.resp.headers.link);
+        var _max_id = RegExp.$1;
+
+        // if max_id available, process getFollowings recursively.
+        if(_max_id != 'https:')
+        {
+            getFollowings(id, accounts, _max_id);
+        }
+        else
+        {
+            console.log("Total accounts: " + accounts.length);
+
+            // if all accounts obtained, proceed to next step, following accounts.
+            followAccounts(accounts);
+        }
+    });
+};
+
+/**
+ * follow accounts not being followed.
+ * @param {array} accounts 
+ */
+var followAccounts = (accounts) => {
+    // set an interval to avoid being DDOS like.
+    setInterval(() => {
+        console.log('Awooooooooo!!!');
+
+        targetM.get('timelines/public', {local:true,limit:STATUS_LIMIT})
         .then(res => {
             let statuses = res.data;
+
             statuses.forEach((status) => {
-                myM.post('follows', {uri: status.account.username + '@' + pref.target.domain})
-                    .then((err, data, res) => {
-                        console.log(err);
-                    });
+                // search if already followed or not.
+                if(!searchFollowing(accounts, status.account))
+                {
+                    accounts.push(status.account);
+
+                    // follow account
+                    setTimeout(() => { 
+                            myM.post('follows', {uri: status.account.username + '@' + pref.target.domain})
+                        .then((res) => {
+                            console.log("Followed: " + res.data.username);
+                        });
+                    }, 500);
+                }
             });
         });
-}, interval_min * 60 * 1000);
+    }, INTERVAL_MIN * 20 * 1000);
+};
+
+/**
+ * search account already followed
+ * @param {array} accounts 
+ * @param {object} account 
+ */
+var searchFollowing = (accounts, account) => {
+    var found = false;
+    accounts.some((_account) => {
+        if(_account.id == account.id)
+        {
+            found = true;
+            return true;
+        }
+    });
+    return found;
+};
